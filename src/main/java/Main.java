c78ff480e15802decc12dc5cd7a69e4176434cee
jaymee54 <Jamielomas54@yahoo.co.uk>
2022-10-13 10:54:25 +0100

    Maven set up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-package org.example;
-
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -1,5 +1,3 @@
-package org.example;
-
 public class Tile {
     boolean Bomb;
 
